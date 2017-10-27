f0792ec5668cf487a9dbe0943d164de6ef25c3f4
hunto <2623611740@qq.com>
2017-10-27 14:33:08 +0800

    Add settings canvas

---
+++ ChickenShooter/Assets/Scripts/Login/MainCanvas.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MainCanvas : MonoBehaviour {
+
+	public Canvas settingsCanvas;
+	public Button settingsButton;
+	public Button closeButton;
+
+	// Use this for initialization
+	void Start () {
+		settingsCanvas.gameObject.SetActive(false);
+		settingsButton.onClick.AddListener (OnSettingsButtonClick);
+		closeButton.onClick.AddListener (OnCloseButtonClick);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	private void OnSettingsButtonClick () {
+		settingsCanvas.gameObject.SetActive(true);
+	}
+
+	private void OnCloseButtonClick () {
+		Application.Quit ();
+	}
+}
---
+++ ChickenShooter/Assets/Scripts/Login/SettingsCanvas.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SettingsCanvas : MonoBehaviour {
+	public Canvas settingsCanvas;
+	public Button saveButton;
+	public Button cancelButton;
+
+	// Use this for initialization
+	void Start () {
+		saveButton.onClick.AddListener (OnSaveButtonClick);
+		cancelButton.onClick.AddListener (OnCancelButtonClick);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	private void OnSaveButtonClick () {
+		// TODO: save sth.
+		SaveSettings();
+		settingsCanvas.gameObject.SetActive(false);
+	}
+
+	private void OnCancelButtonClick () {
+		settingsCanvas.gameObject.SetActive(false);
+	}
+
+	private void SaveSettings () {
+	}
+}
