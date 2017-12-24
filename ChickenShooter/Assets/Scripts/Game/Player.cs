df38f3197ffe9c5603d84d76ccc4eef5e35ef1f5
hunto <2623611740@qq.com>
2017-12-24 16:48:48 +0800

    日常更新

--- ChickenShooter/Assets/Scripts/Game/Player.cs
+++ ChickenShooter/Assets/Scripts/Game/Player.cs
@@ -4,54 +4,53 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 
-public class Player : NetworkBehaviour {
-
-	public Transform target;//要跟随的目标
-	public float distance=0f;//摄像机离目标的距离
-	public float height=0.0f;//摄像机离目标的高度
-	public float heightDamping=0f;//水平跟随平滑系数
-	public float rotationDamping=0f;//跟随高度变化系数
-	public float refRotation=0f;
-	public float refHeight=0f;
+public class Player : NetworkBehaviour
+{
 
 	// Exposed Parameters
 	public int life = 5;
 
 	// Component References
-	Transform playerTransform;
-	CharacterController controller;
-	Animator anim;
+	Transform	playerTransform;
+	CharacterController	controller;
+	Animator	anim;
+	Weapon	weapon;
 
 	// Camera : Component
 	Transform camTransform;
-	// float camHeight = 1.4f;
 	Vector3 camOffset = new Vector3(1f, 4.6f, -2.2f);
-	// Vector3 camOffset = new Vector3(0f, 4.5f, 0f);
+	// third person
+	// Vector3 camOffset = new Vector3(0f, 4.5f, 0f); // first person
 
 	// Attributes
-	float 	walkSpeed = 4f;
-	float  	runSpeed = 14;
-	float		gravity = 2f;
-	// States
-	bool		isJogging = false;
-	bool 		isRunning = false;
-	bool 		isAiming = false;
+	float	walkSpeed = 4f;
+	float	runSpeed = 14;
+	float	gravity = 2f;
+	// States - for animations
+	bool	isJogging = false;
+	bool	isRunning = false;
+	bool	isAiming = false;
 
 
 
-	void Start () {
-		/*	set references */
+	void Start()
+	{
+		/* set references */
 		if (!isLocalPlayer) {
 			return;
 		}
 		// Transform : Player
 		playerTransform = this.transform;
 		// CharCont : Player
-		controller = this.GetComponent <CharacterController> ();
+		controller = this.GetComponent<CharacterController>();
 		// Transform : Camera
 		camTransform = Camera.main.transform;
 		// Animator
-		anim = GetComponent <Animator> ();
+		anim = GetComponent <Animator>();
+		// Weapon
+		// TODO: temporary solution
+		//       should be handled by dedicated PickUpWeapon function
+		weapon = this.GetComponentInChildren(typeof(Weapon)) as Weapon;
 
 		// Set Camera start position
 		// set rotation
@@ -61,102 +60,92 @@ public class Player : NetworkBehaviour {
 		camTransform.position = playerTransform.position;
 		camTransform.localPosition += camOffset;
 
+
+
 		// Lock mouse
 		Cursor.lockState = CursorLockMode.Locked;
-	}	// Start
+	}
+	// Start
 
 
-	void Update () {
+	void Update()
+	{
 
-		if (life <= 0 || !isLocalPlayer) { return; }
+		if (life <= 0 || !isLocalPlayer) {
+			return;
+		}
 		Contorl();
 		Animate();
-		FireBullet();	// DEBUG
-	}	// Update
-
-
-	/*
-	void cameraUpdate (Transform target, Transform transform) {
-			if(target){
-				float targetRotationAngle=target.eulerAngles.y;//目标的朝向
-				float targetHeight=target.position.y+height;//得到跟随的高度
-
-				float cameraRotationAngle=transform.eulerAngles.y;//摄像机的朝向
-				float cameraHeight=transform.position.y;//摄像机的高度
-
-				cameraRotationAngle=Mathf.SmoothDampAngle(cameraRotationAngle,targetRotationAngle,ref refRotation,rotationDamping);//从摄像机目前的角度变换到目标的角度
-
-				cameraHeight=Mathf.SmoothDamp(cameraHeight,targetHeight,ref refHeight,heightDamping);//从摄像机目前的高度平滑变换到目标的高度
-
-				Quaternion cameraRotation=Quaternion.Euler(0,cameraRotationAngle,0);//每帧在Y轴上旋转摄像机 旋转的角度为cameraRotationAngle 因为上面的代码已经得到了每帧要从摄像机当前的角度变换到目标角度cameraRotationAngle
-
-				//下面几句代码主要设置摄像机的位置
-				transform.position=target.position;
-				transform.position-= cameraRotation * Vector3.forward * distance;
-				transform.position=new Vector3(transform.position.x, cameraHeight,transform.position.z);
-
-				//使摄像机一直朝着目标方向
-				transform.LookAt(target);
-			}
 	}
-	*/
+	// Update
 
 
-	void Contorl() {
-		/*	contorl player movement */
+
+	void Contorl()
+	{
+		/*    contorl player movement */
 
 		//// Player ////
 		// Set Transform.position
 		// Set player Transform
 		float x = 0f, y = 0f, z = 0f;
-		 if (Input.GetKey(KeyCode.Space)) { y += 1f;}
-		 if (Input.GetKey(KeyCode.LeftControl)) { y -= 1f; }
+		if (Input.GetKey(KeyCode.Space)) {
+			y += 1f;
+		}
+		if (Input.GetKey(KeyCode.LeftControl)) {
+			y -= 1f;
+		}
 		if (Input.GetKey(KeyCode.LeftShift)) {
-			if (!isRunning) { isRunning = true; }
+			if (!isRunning) {
+				isRunning = true;
+			}
 			y -= gravity * Time.deltaTime;
 			z += Input.GetAxisRaw("Vertical") * runSpeed * Time.deltaTime;
 			x += Input.GetAxisRaw("Horizontal") * runSpeed * Time.deltaTime;
 		} else {
-			if (isRunning) { isRunning = false; }
+			if (isRunning) {
+				isRunning = false;
+			}
 			y -= gravity * Time.deltaTime;
 			z += Input.GetAxisRaw("Vertical") * walkSpeed * Time.deltaTime;
 			x += Input.GetAxisRaw("Horizontal") * walkSpeed * Time.deltaTime;
 		}
 		// assign
-		controller.Move(
-				playerTransform.TransformDirection(new Vector3(x, y, z)));
+		controller.Move(playerTransform.TransformDirection(new Vector3(x, y, z)));
 
-		if (x!=0 || z!=0) { isJogging = true; }
-		else { isJogging = false; }
+		if (x != 0 || z != 0) {
+			isJogging = true;
+		} else {
+			isJogging = false;
+		}
 
 		Vector3 newPlayerEuler = playerTransform.eulerAngles;
 		newPlayerEuler.y += Input.GetAxis("Mouse X");
 		playerTransform.eulerAngles = newPlayerEuler;
 
 		//// Camera ////
-		// set rotation
-		// camTransform.rotation = playerTransform.rotation;
 		// set position
 		camTransform.position = playerTransform.position;
 		camTransform.localPosition += camOffset;
+		// set rotation
 		camTransform.eulerAngles = new Vector3(
-				camTransform.localEulerAngles.x - Input.GetAxis("Mouse Y"),
-				playerTransform.eulerAngles.y - 0f, // character not holding firmly
-				0f // playerTransform.EulerAngles.z
+			camTransform.localEulerAngles.x - Input.GetAxis("Mouse Y"),
+			playerTransform.eulerAngles.y - 0f, // character not holding firmly
+			0f // playerTransform.EulerAngles.z
 		);
 
-
-		//cameraUpdate (playerTransform, camTransform);
-
 		// Mouse Click
-		if (!isAiming && Input.GetKeyDown(KeyCode.Mouse1)) { isAiming = true; }
-		else if (isAiming && Input.GetKeyUp(KeyCode.Mouse1)) { isAiming = false; }
-	}	// def Contorl
+		if (!isAiming && Input.GetKeyDown(KeyCode.Mouse1)) {
+			isAiming = true;
+		} else if (isAiming && Input.GetKeyUp(KeyCode.Mouse1)) {
+			isAiming = false;
+		}
+	}
 
 
 
-	void Animate() {
-
+	void Animate()
+	{
 		anim.SetBool("IsJogging", isJogging);
 		anim.SetBool("IsAiming", isAiming);
 		anim.SetBool("IsRunning", isRunning);
@@ -164,36 +153,21 @@ public class Player : NetworkBehaviour {
 			anim.SetTrigger("Fire");
 			// FireBullet();
 		}
+		// check if death
+		if (life < 0) {
+			anim.SetTrigger("Death");
+		}
 	}
 
 
 
-	GameObject FireBullet() {
-		/**	call this after a bullet is fired
-		Param:	void
-		Return:	the <GameObject> that is hit || null
-		*/
-
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		RaycastHit hit;
-
-		if (Physics.Raycast(ray, out hit, 100)) {
-			Debug.DrawLine(ray.origin, hit.point);
-			Debug.Log(hit.collider.gameObject);
-			return hit.collider.gameObject;
-		}
-
-		return null;
-
-	}	// FireBullet
-
-
-
-
-	void OnDrawGizmos() {
+	void OnDrawGizmos()
+	{
 
 		Gizmos.DrawIcon(this.transform.position, "Spawn.tif");
-	}	// OnDrawGizmos
+	}
+	// OnDrawGizmos
 
 
-}	// public class Player
+}
+// public class Player
--- ChickenShooter/Assets/Scripts/Game/Weapon.cs
+++ ChickenShooter/Assets/Scripts/Game/Weapon.cs
@@ -3,46 +3,83 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Weapon : MonoBehaviour {
+public class Weapon : MonoBehaviour
+{
 
-	public GameObject playerRightHand;
+	public GameObject playerRightHand;	// TODO: temporary
 	public GameObject flame;
 
 	float flameDuration = -1f;
 	public float fireRate = -1f;
 	GameObject clone_flame;
+	Player player;
 
 
-	void Awake() {
-
+	void Awake()
+	{
 		this.transform.parent = playerRightHand.transform;
+		player = this.GetComponentInParent<Player>();		// TODO: temporary
 	}
 
 
-	// Update is called once per frame
-	void Update () {
 
-		// GunFlare_FX
-		if (/*Input.GetKey(KeyCode.Mouse1)	// can't fire before aiming
-				&&*/ Input.GetKeyDown(KeyCode.Mouse0)
-				&& fireRate < 0) {
-			clone_flame = Instantiate(flame, this.transform.position,
-					this.transform.rotation, this.transform) as GameObject;
-			clone_flame.transform.localPosition += new Vector3(0, 0, 60f);
-			flameDuration = 0.13f;
-			fireRate = 0.53f;
+	void PlayFireAnimation(Player player)
+	{
+		clone_flame = Instantiate(flame, this.transform.position,
+			this.transform.rotation, this.transform) as GameObject;
+		clone_flame.transform.localPosition += new Vector3(0, 0, 60f);
+	}
+
+
+
+	GameObject CheckBulletHit(Player player)
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray, out hit, 100)) {
+			Debug.DrawLine(ray.origin, hit.point);
+			Debug.Log(hit.collider.gameObject);
+
+			// causing damage
+			if (hit.collider.gameObject.CompareTag("Player")) {
+				Player other = hit.collider.gameObject.GetComponent(typeof(Player)) as Player;
+				other.life -= 1;
+				Debug.Log(other);
+			}
+			return hit.collider.gameObject;
 		}
+		return null;
+	}
 
-		if (flameDuration > 0f)	{
+
+
+	void Update()
+	{
+		// be careful of memory leaks!
+		if (flameDuration > 0f) {
 			flameDuration -= Time.deltaTime;
 		} else if (clone_flame != null) {
 			Destroy(clone_flame.gameObject);
 		}
-
+		// control fire rate
 		if (!(fireRate < 0f)) {
 			fireRate -= Time.deltaTime;
 		}
-	}	// Update
+
+		// don't do anything if the guns not pick up
+		if (player == null) {
+			return;
+		}
+
+		// fire gun
+		if (Input.GetKeyDown(KeyCode.Mouse0) && fireRate < 0) {
+			PlayFireAnimation(player);	// play vfx
+			CheckBulletHit(player);		// check if hit something
+			flameDuration = 0.13f;
+			fireRate = 0.53f;
+		}
+	}
 
 
 }
