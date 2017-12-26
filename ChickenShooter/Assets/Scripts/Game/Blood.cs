194ab6dfb4c7afe007b1c660c831ea0f6453905a
hunto <2623611740@qq.com>
2017-12-26 16:31:25 +0800

    新增血条，大版本更新前备份

---
+++ ChickenShooter/Assets/Scripts/Game/Blood.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blood: MonoBehaviour {
+    public Slider lifeSlider;
+    public Image BloodImage;
+    public float process = 0.7f;
+    // Use this for initialization
+    void Start () {
+        lifeSlider.value = process;
+        BloodImage.color = new Color(0.5f, 0.5f, 0.5f, 1);
+    }
+	
+	void Update () {
+
+    }
+    public void SetProgress(float progress)
+    {
+        lifeSlider.value = progress;
+        if (process > 0.99)
+        {
+            BloodImage.color = new Color(0.5f, 0.5f, 0.5f, 1);
+        }
+        else
+        {
+            BloodImage.color = new Color(0.5f + 0.3f * (1 - process), 0.5f - 0.3f * (1 - process), 0.5f - 0.3f * (1 - process), 1);
+        }
+    }
+}
